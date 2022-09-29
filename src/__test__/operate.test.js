5d76c4637b7a81dad8b31942bb553c840df87228
Bantealem <bantem2021@gmail.com>
2022-09-29 16:12:55 +0300

    update test

--- src/__test__/operate.test.js
+++ src/__test__/operate.test.js
@@ -14,7 +14,7 @@ describe('The opretaion should return the exact value', () => {
     test('should return division of the two number', () => {
         expect(operate(4, 2, '÷')).toBe("2")
     });
-    test('should return division of the two number', () => {
+    test('should return modular of the two number', () => {
         expect(operate(100, 10, '%')).toBe("0")
     });
 
